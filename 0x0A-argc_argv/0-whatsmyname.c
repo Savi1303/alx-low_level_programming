9200779910593a8d96fb4e281ad5666fbf18c36e
Smart Saviour <zsavizfesh@gmail.com>
2023-10-28 02:52:45 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,32 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
-*main - adds positive numbers
+* main - prints program name
 *@argc: number of arguments
 *@argv: array of arguments
-*Return: 0 on success, 1 on failure
+*Return: 0
 */
-
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int i, j, sum = 0;
-
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-
-		sum += atoi(argv[i]);
-	}
-
-	printf("%d\n", sum);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
