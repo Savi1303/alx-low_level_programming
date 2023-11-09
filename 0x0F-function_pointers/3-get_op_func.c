68d45ac99bae68c78be0d1d70a1dd60d5429d96d
Smart Saviour <zsavizfesh@gmail.com>
2023-11-09 01:59:56 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,36 +1,31 @@
 #include "3-calc.h"
-/**
- * get_op_func - function that selects the correct function to perform the
- *               operation asked by the user.
- *
- * @s: This is the input operator
- *
- * Return: This function returns a pointer to the function that corresponds to
- *         the operator given as a parameter. Example: get_op_func("+")
- *         should return a pointer to the function op_add"")
- *         If s does not match any of the 5 expected operators
- *         (+, -, *, /, %),return NULL)
- *
- */
-int (*get_op_func(char *s))(int a, int b)
-{
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+#include <stdlib.h>
 
-	while (ops[i].op != NULL)
-	{
-		if (*ops[i].op == *s)
-		{
-			return (ops[i].f);
-		}
-		i++;
-	}
-	return (NULL);
+/**
+*get_op_func - selects the correct function to perform
+*the operation asked by the user. You’re not allowed
+*to declare any other function.
+*@s: operator passed as argument
+*
+*Return: A pointer to the function corresponding to
+*the operator given in parameter
+*/
+int (*get_op_func(char *s))(int, int)
+{
+
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+
+return (ops[i].f);
+
 }
